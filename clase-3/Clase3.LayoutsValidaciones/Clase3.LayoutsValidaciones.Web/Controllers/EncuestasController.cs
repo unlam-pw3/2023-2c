0be44bd30068893d9f83b151bca3643386d21734
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2023-09-06 20:12:57 -0300

    agregando _OceanoLayout.cshtml y _BaseLayout.cshtml

---
+++ clase-3/Clase3.LayoutsValidaciones/Clase3.LayoutsValidaciones.Web/Controllers/EncuestasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clase3.LayoutsValidaciones.Web.Controllers
+{
+    public class EncuestasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ clase-3/Clase3.LayoutsValidaciones/Clase3.LayoutsValidaciones.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Clase3.LayoutsValidaciones.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
