bf07c5e4bf22d07ddeb54abe8367031247028220
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2023-09-20 20:49:45 -0300

    agregando vista CalcularAreaRectangulo y validaciones

---
+++ clase-5/Clase5.Modelo1erParcial/Clase5.Modelo1erParcial.Logica/Rectangulo.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Clase5.Modelo1erParcial.Logica;
+
+public class Rectangulo
+{
+    [Required]
+    [Range(1,99, ErrorMessage = "Base debe estar en un rango: 0 < x < 100.")]
+    public int? Base { get; set; }
+    
+    [Required]
+    [Range(1,99, ErrorMessage = "Base debe estar en un rango: 0 < x < 100.")]
+    public int? Altura { get; set; }
+    public int Area => (Base ?? 0) * (Altura ?? 0);
+}
--- clase-5/Clase5.Modelo1erParcial/Clase5.Modelo1erParcial.Web/Controllers/PrincipalController.cs
+++ clase-5/Clase5.Modelo1erParcial/Clase5.Modelo1erParcial.Web/Controllers/PrincipalController.cs
@@ -1,10 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Clase5.Modelo1erParcial.Logica;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Clase5.Modelo1erParcial.Web.Controllers
 {
     public class PrincipalController : Controller
     {
         public IActionResult Bienvenido()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CalcularAreaRectangulo()
+        {
+            return View(new Rectangulo());
+        }
+
+        [HttpPost]
+        public IActionResult CalcularAreaRectangulo(Rectangulo request)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Resultados");
+            }
+            
+            return View(request);
+        }
+
+        [HttpGet]
+        public IActionResult Resultados()
         {
             return View();
         }
