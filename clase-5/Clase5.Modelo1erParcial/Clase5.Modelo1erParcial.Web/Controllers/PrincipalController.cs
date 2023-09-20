2d7f2eeae35f37e997f299370e1943a9d419b40c
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2023-09-20 20:30:19 -0300

    agregando modelo parcial, quitando todos los archivos que no van y agregando el punto 1) de bienvenido y layout

---
+++ clase-5/Clase5.Modelo1erParcial/Clase5.Modelo1erParcial.Web/Controllers/PrincipalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clase5.Modelo1erParcial.Web.Controllers
+{
+    public class PrincipalController : Controller
+    {
+        public IActionResult Bienvenido()
+        {
+            return View();
+        }
+    }
+}
