ce745849f79938e197ca018303f2637cb06879ee
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2023-10-11 21:13:19 -0300

    agregando testing a agregar tesoro

---
+++ clase-6/Clase6.EF/Clase6.EF.Test/Comun/TestBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clase6.EF.Data.EF;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clase6.EF.Test.Comun
+{
+    public class TestBase
+    {
+        protected ServiceCollection Services { get; }
+        protected ServiceProvider ServiceProvider { get; private set; }
+
+        public TestBase()
+        {
+            Services = new ServiceCollection();
+
+            Services.AddEntityFrameworkInMemoryDatabase();
+
+            Services.AddDbContext<Pw32cIslaTesoroContext>((sp, options) =>
+                options.UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .UseInternalServiceProvider(sp));
+
+            ServiceProvider = Services.BuildServiceProvider();
+        }
+    }
+}
